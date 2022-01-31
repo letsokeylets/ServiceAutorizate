0677dead575dd5c51cb1959495a84f84846d2949
Mozdir Taras <letsokey@mail.ru>
2022-01-31 23:21:00 +0400

    Last refactoring

--- src/main/java/ru/netology/serviceautorizate/repository/UserRepository.java
+++ src/main/java/ru/netology/serviceautorizate/repository/UserRepository.java
@@ -15,8 +15,11 @@ import static ru.netology.serviceautorizate.controller.Authorities.WRITE;
 @Repository
 public class UserRepository {
 
+    private static final String USER = "admin";
+    private static final String PASSWORD = "1234";
+
     public List<Authorities> getUserAuthorities(User user) {
-        if (user.getUser().equals("admin") && user.getPassword().equals("1234"))
+        if (user.getUser().equals(USER) && user.getPassword().equals(PASSWORD))
             return Arrays.asList(READ, WRITE, DELETE);
         return new ArrayList<>();
     }
